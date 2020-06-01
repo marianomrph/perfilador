166467ad4660a980cdb5f4f7ffb58f90ce700b7c
Juan Manuel Vallejo <jmvallejo@gmail.com>
2020-06-01 15:57:10 -0500

    Add initial Jackpot logic

--- src/components/Jackpot.jsx
+++ src/components/Jackpot.jsx
@@ -1,28 +1,114 @@
-import React, { Component } from 'react'
-import classNames from 'classnames'
-import JackpotItem from './JackpotItem'
-import './Jackpot.scss'
+import React, { Component } from 'react';
+import classNames from 'classnames';
+import JackpotItem from './JackpotItem';
+import './Jackpot.scss';
+
+const ROTATION_INTERVAL_MILLISECONDS = 100;
+const TOTAL_JACKPOT_DURATION_MILLISECONDS = 3000;
+
+const TIME_OPTIONS = [
+	'En los años 1600',
+	'La época de los dinosaurios',
+	'El futuro lejano',
+	'La segunda guerra mundial'
+];
+
+const SPACE_OPTIONS = [
+	'Argentina',
+	'Europa, una luna de Júpiter',
+	'Titán, una luna de Saturno',
+	'Un bosque encantado'
+];
+
+const ADJECTIVE_OPTIONS = [ 'Sombrío', 'Caliente', 'Frío', 'Húmedo', 'Rápido' ];
 
 class Jackpot extends Component {
-  render () {
-    return (
-      <div className='jackpot-container'>
-        <div className='jackpot-container-items'>
-          <JackpotItem defaultLabel='Tiempo' />
-          <JackpotItem defaultLabel='Espacio' />
-          <JackpotItem defaultLabel='Adjetivo' />
-        </div>
-        <div className='jackpot-button'>
-          <div className='note'>
-            <div>Probá al azar!</div>
-            <div className='arrow' />
-          </div>
-          <div className='circle' />
-          <div className='line' />
-        </div>
-      </div>
-    )
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			timeLabel: '',
+			spaceLabel: '',
+			adjectiveLabel: ''
+		};
+	}
+
+	componentWillUnmount() {
+		this.stopJackpot();
+	}
+
+	startJackpot() {
+		this.stopJackpot();
+		this._jackpotTimeout = setTimeout(this.stopJackpot.bind(this), TOTAL_JACKPOT_DURATION_MILLISECONDS);
+		this._rotationInterval = setInterval(this.randomRotation.bind(this), ROTATION_INTERVAL_MILLISECONDS);
+	}
+
+	stopJackpot() {
+		if (this._jackpotTimeout) {
+			clearTimeout(this._jackpotTimeout);
+		}
+
+		if (this._rotationInterval) {
+			clearInterval(this._rotationInterval);
+		}
+	}
+
+	randomRotation() {
+		// Get random timeLabel
+		const randomTimeLabelIndex = Math.floor(Math.random() * TIME_OPTIONS.length);
+		// Get random spaceLabel
+		const randomSpaceLabelIndex = Math.floor(Math.random() * TIME_OPTIONS.length);
+		// Get random adjectiveLabel
+		const randomAdjectiveLabelIndex = Math.floor(Math.random() * TIME_OPTIONS.length);
+
+		this.setState({
+			timeLabel: TIME_OPTIONS[randomTimeLabelIndex],
+			spaceLabel: SPACE_OPTIONS[randomSpaceLabelIndex],
+			adjectiveLabel: ADJECTIVE_OPTIONS[randomAdjectiveLabelIndex]
+		});
+	}
+
+	onSelect(labelName, value) {
+		this.setState({
+			[labelName]: value
+		});
+	}
+
+	render() {
+		const { timeLabel, spaceLabel, adjectiveLabel } = this.state;
+
+		return (
+			<div className="jackpot-container">
+				<div className="jackpot-container-items">
+					<JackpotItem
+						defaultLabel="Tiempo"
+						optionLabel={timeLabel}
+						options={TIME_OPTIONS}
+						onSelect={this.onSelect.bind(this, 'timeLabel')}
+					/>
+					<JackpotItem
+						defaultLabel="Espacio"
+						optionLabel={spaceLabel}
+						options={SPACE_OPTIONS}
+						onSelect={this.onSelect.bind(this, 'spaceLabel')}
+					/>
+					<JackpotItem
+						defaultLabel="Adjetivo"
+						optionLabel={adjectiveLabel}
+						options={ADJECTIVE_OPTIONS}
+						onSelect={this.onSelect.bind(this, 'adjectiveLabel')}
+					/>
+				</div>
+				<div className="jackpot-button" onClick={this.startJackpot.bind(this)}>
+					<div className="note">
+						<div>Probá al azar!</div>
+						<div className="arrow" />
+					</div>
+					<div className="circle" />
+					<div className="line" />
+				</div>
+			</div>
+		);
+	}
 }
 
-export default Jackpot
+export default Jackpot;
--- src/components/JackpotItem.jsx
+++ src/components/JackpotItem.jsx
@@ -1,55 +1,62 @@
-import React, { Component } from 'react'
-import classNames from 'classnames'
-import './Jackpot.scss'
+import React, { Component } from 'react';
+import classNames from 'classnames';
+import './Jackpot.scss';
 
 class JackpotItem extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      selectOpen: false
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			selectOpen: false
+		};
+	}
 
-  open () {
-    this.setState({
-      selectOpen: true
-    })
-  }
+	open() {
+		this.setState({
+			selectOpen: true
+		});
+	}
 
-  close () {
-    this.setState({
-      selectOpen: false
-    })
-  }
+	close() {
+		this.setState({
+			selectOpen: false
+		});
+	}
 
-  render () {
-    const { selectOpen } = this.state
-    const { defaultLabel, optionLabel } = this.props
-    return (
-      <div className='jackpot-select'>
-        <div className='jackpot-item' onClick={this.open.bind(this)}>{defaultLabel} <div className='chevron' /></div>
-        { selectOpen &&
-          <div className='jackpot-modal'>
-            <div className='jackpot-modal'>
-              <div className='jackpot-modal-header'>
-                <div>Elejí una opción</div>
-                <div className='close' onClick={this.close.bind(this)} />
-              </div>
-              <ul>
-                <li>Opción 01</li>
-                <li>Opción 01</li>
-                <li>Opción 01</li>
-                <li>Opción 01</li>
-                <li>Opción 01</li>
-                <li>Opción 01</li>
-                <li>Opción 01</li>
-              </ul>
-            </div>
-          </div>
-        }
-      </div>
-    )
-  }
+	selectAndClose(option) {
+		const { onSelect } = this.props;
+		onSelect(option);
+		this.close();
+	}
+
+	render() {
+		const { selectOpen } = this.state;
+		const { defaultLabel, options, optionLabel } = this.props;
+		return (
+			<div className="jackpot-select">
+				<div className="jackpot-item" onClick={this.open.bind(this)}>
+					{optionLabel || defaultLabel} <div className="chevron" />
+				</div>
+				{selectOpen && (
+					<div className="jackpot-modal">
+						<div className="jackpot-modal">
+							<div className="jackpot-modal-header">
+								<div>Elejí una opción</div>
+								<div className="close" onClick={this.close.bind(this)} />
+							</div>
+							{options &&
+							options.length && (
+								<ul>
+									{options.map((option) => (
+										<li onClick={() => this.selectAndClose(option)}>{option}</li>
+									))}
+								</ul>
+							)}
+						</div>
+					</div>
+				)}
+			</div>
+		);
+	}
 }
 
-export default JackpotItem
+export default JackpotItem;
